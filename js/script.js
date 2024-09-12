17ca1bfcd39971d7baaf4e7bf94b5ec88be66680
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-09-11 20:26:30 -0700

    WIP changing views in mobile

--- js/script.js
+++ js/script.js
@@ -4,5 +4,18 @@ import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 // );
 const dataResponse = await fetch("data.json");
 const data = await dataResponse.json();
-
+const previewBtns = document.querySelectorAll("[data-preview]");
 console.log(data);
+
+console.log(previewBtns);
+function togglePreview(e) {
+  //   console.log(e.currentTarget.dataset.preview);
+  for (const previewBtn of previewBtns) {
+    const view = e.currentTarget.closest("div[data-show]");
+    view.setAttribute("data-show");
+  }
+}
+
+for (const previewBtn of previewBtns) {
+  previewBtn.addEventListener("click", togglePreview);
+}
